8f26c58ef8e59188f90517f7a9498377a6b133b6
gabe927 <gabe927@gmail.com>
2022-08-26 10:07:10 -0400

    first commit

---
+++ f.py
@@ -0,0 +1,26 @@
+import time
+import keyboard
+import threading
+import sys
+
+def run():
+    while True:
+        keyboard.press_and_release('enter')
+        time.sleep(0.1)
+        keyboard.press_and_release('f')
+        time.sleep(0.1)
+
+# wait for f to start
+print("Press F to start paying respects")
+keyboard.wait('f')
+print("Paying respects")
+
+# start run thread
+t = threading.Thread(target=run)
+t.daemon = True
+t.start()
+
+# kill on esc
+keyboard.wait('esc')
+print("Done paying repsects")
+sys.exit()
